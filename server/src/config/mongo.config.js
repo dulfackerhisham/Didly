d8e6e473f7bec998e77d3d1d0e1b898fc0b3dc60
Hisham Dulfacker <dulfackerhisham@gmail.com>
2025-10-23 12:32:34 +0530

    glitch

--- server/src/config/mongo.config.js
+++ server/src/config/mongo.config.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose'
 
 export const connectDb = async () => {
     try {
-        console.log("MONGO_URI:", process.env.MONGO_URI);
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`✅ Database Connected: ${conn.connection.host}`);
     } catch (err) {
