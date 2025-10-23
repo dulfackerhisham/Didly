ca4b1db6b9b6d11e538fa5f78d9b56547ecd594b
Hisham Dulfacker <dulfackerhisham@gmail.com>
2025-10-23 10:40:50 +0530

    nanoid configured and sample api

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import { nanoid } from 'nanoid'
 
 const app = express();
 const PORT = 5000
 
+app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+
+
+app.post("/api/create", (req, res) => {
+    const {url} = req.body
+    console.log(`url -> ${url}`);
+    res.send(nanoid(15))
+})
+
 app.get("/", (req, res) => {
-    res.send("Hello Hisham")
+    const url = req.body
+    console.log(url);
+    
+    res.send(nanoid(5))
 })
 
 app.listen(PORT, () => {
